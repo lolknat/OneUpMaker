2dfcafce95da04419defef46fbf68a255eda1449
fc58435 <fc58435@alunos.fc.ul.pt>
2025-08-14 11:45:42 +0100

    updated toString to use Cell matrix and placeWalls revamp

--- Create/OneUpGen.java
+++ Create/OneUpGen.java
@@ -22,39 +22,42 @@ public class OneUpGen {
     }
 
     //important methods
-    public void placeWalls(int qty) {
-        for (int i = 0; i < qty; i++) {
-            int rnd = (int) (Math.random() * (size * size));
-            int row = rnd / size;
-            int col = rnd % size;
 
-            if (Math.random() < 0.5) {
-                if (col < size - 1) {
-                    currBoard[row][col].wr(true);
-                    if (isSolvable(currBoard)) {
-                        origBoard[row][col].wr(true);
-                    } else {
-                        currBoard[row][col].wr(false); // undo
-                        i--;
-                    }
-                } else {
-                    i--;
-                }
-            } else {
-                if (row < size - 1) {
-                    currBoard[row][col].wd(true);
-                    if (isSolvable(currBoard)) {
-                        origBoard[row][col].wd(true);
-                    } else {
-                        currBoard[row][col].wd(false); // undo
-                        i--;
-                    }
-                } else {
-                    i--;
-                }
+    /**
+     * randomly places walls on the board in a way that it's still solveable
+     * 
+     * @param qty number of wall pairs (for each vertical wall there's a horizontal wall)
+     */
+    public void placeWalls(int qty) {
+        Cell[][] testBoard = currBoard.clone();
+        int cnt = 0;
+        while (cnt < qty) {
+            //place random vertical wall
+            int vWall = (int) (Math.random() * (size * (size-1)));
+            int vRow = vWall / size;
+            int vCol = vWall % (size-1); //dont place wall on edges
+            testBoard[vRow][vCol].wr(true);
+
+            //place random horizontal wall
+            int hWall = (int) (Math.random() * ((size-1) * size));
+            int hRow = hWall / (size-1); //dont place wall on edges
+            int hCol = hWall / size;
+            testBoard[vRow][vCol].wd(true);
+
+            //check if possible or go back
+            if (isSolvable(currBoard)){
+                currBoard[vRow][vCol].wr(true);
+                currBoard[hRow][hCol].wd(true);
+            }else{ //go back
+                testBoard[vRow][vCol].wr(false);
+                testBoard[hRow][hCol].wd(false);
+                cnt--;
             }
+
+            cnt++;
         }
     }
+    
 
     public Cell[][] addWalls(Cell[][] board, int row, int col, boolean right, boolean down){
         if(right){
@@ -68,10 +71,36 @@ public class OneUpGen {
 
     public boolean isSolvable(Cell[][] board){
         return true; //TODO
+        //what if i fill every cell with a list of possible values according to rules and then check the possibilities?
     }
 
 
     //secondary methods
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (Cell[] r:currBoard){
+            //to represent numbers and row walls
+            for (Cell c:r){
+                if (c.wr()){
+                    sb.append(c.num()+"|");
+                }else{
+                    sb.append(c.num()+" ");
+                }
+            }
+            sb.append("\n");
+
+            //to represent col walls
+            for (Cell c2:r){
+                if (c2.wd()){
+                    sb.append("- ");
+                }else{
+                    sb.append("  ");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
     /*public String toString(){
         int[][] b = currBoard;
         boolean[][] rw = rowWalls;
